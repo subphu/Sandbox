13db56d8312d4c657f659743ce9250952065e7ac
Subroto <subrotoph@gmail.com>
2021-09-20 17:15:37 +0900

    Create basic shader

---
+++ sources/shaders/compile.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+shader_dir="$SRCROOT/sources/shaders"
+spirv_dir="$SRCROOT/resources/spirv"
+glslc_dir="$SRCROOT/libraries/vulkansdk-mac/bin"
+
+echo "shader dir: $shader_dir"
+echo "spirv dir: $spirv_dir"
+echo "glslc dir: $glslc_dir"
+
+$glslc_dir/glslc $shader_dir/shader.vert -o $spirv_dir/vert.spv
+$glslc_dir/glslc $shader_dir/shader.frag -o $spirv_dir/frag.spv
+$glslc_dir/glslc $shader_dir/shader.comp -o $spirv_dir/comp.spv
