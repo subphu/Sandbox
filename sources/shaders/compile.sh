89097817588142c24d317a702f858d27575fd2eb
Subroto <subrotoph@gmail.com>
2021-11-14 22:20:45 +0900

    Add shaders for interference

--- sources/shaders/compile.sh
+++ sources/shaders/compile.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-shader_dir="$SRCROOT/sources/shaders"
 spirv_dir="$SRCROOT/resources/spirv"
 glslc_dir="$SRCROOT/libraries/vulkansdk-mac/bin"
+
+shader_dir="$SRCROOT/sources/shaders"
+compute_dir="$shader_dir/compute"
 
 echo "shader dir: $shader_dir"
 echo "spirv dir: $spirv_dir"
@@ -14,3 +16,6 @@ $glslc_dir/glslc $shader_dir/shader.comp -o $spirv_dir/comp.spv
 
 $glslc_dir/glslc $shader_dir/swapchain.vert -o $spirv_dir/swapchain.vert.spv
 $glslc_dir/glslc $shader_dir/swapchain.frag -o $spirv_dir/swapchain.frag.spv
+
+$glslc_dir/glslc $compute_dir/interference1d.comp -o $spirv_dir/interference1d.spv
+$glslc_dir/glslc $compute_dir/interference2d.comp -o $spirv_dir/interference2d.spv
