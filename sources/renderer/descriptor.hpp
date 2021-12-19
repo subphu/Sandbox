e92ffaa5106825f074fb8d2b4d15ce7819df1723
Subroto <subrotoph@gmail.com>
2021-12-19 14:19:21 +0900

    Add more descriptor enum

--- sources/renderer/descriptor.hpp
+++ sources/renderer/descriptor.hpp
@@ -6,9 +6,9 @@
 #include "../include.h"
 #include "../renderer/device.hpp"
 
-enum Set     { S0 = 0, S1 = 1, S2 = 2, S3 = 3, S4 = 4, S5 = 5, S6 = 6, S7 = 7 };
-enum SetIdx  { I0 = 0, I1 = 1, I2 = 2, I3 = 3, I4 = 4, I5 = 5, I6 = 6, I7 = 7 };
-enum Binding { B0 = 0, B1 = 1, B2 = 2, B3 = 3, B4 = 4, B5 = 5, B6 = 6, B7 = 7 };
+enum Set     { S0 = 0, S1 = 1, S2 = 2, S3 = 3, S4 = 4, S5 = 5, S6 = 6, S7 = 7, S8 = 8, S9 = 9 };
+enum SetIdx  { I0 = 0, I1 = 1, I2 = 2, I3 = 3, I4 = 4, I5 = 5, I6 = 6, I7 = 7, I8 = 8, I9 = 9 };
+enum Binding { B0 = 0, B1 = 1, B2 = 2, B3 = 3, B4 = 4, B5 = 5, B6 = 6, B7 = 7, B8 = 8, B9 = 9 };
 
 class Descriptor {
     
